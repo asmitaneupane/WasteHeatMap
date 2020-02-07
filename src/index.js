d0240a33f575c9249277c89c71cbd643ecc0cbf3
Asmita Neupane <neupane.ashmita2056@gmail.com>
2020-02-07 22:54:05 +0545

    add userprofile and integrate menu

--- src/index.js
+++ src/index.js
@@ -2,17 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import './css/index.css';
-import Homepage from './Homepage';
 import * as serviceWorker from './serviceWorker';
 import WasteMap from './WasteMap';
+import Homepage from './Homepage';
+import Menu from './Homepage';
+import UserProfile from './UserProfile';
 
 ReactDOM.render(
     <Router>
-
+        < Menu />
         <Switch>
-
+        
         <Route exact path="/" name="homepage" component={Homepage} />
         <Route path="/wastemap" name="wastemap" component={WasteMap} />
+        <Route path="/userprofile" name="userprofile" component={UserProfile}/>
+        
 
         </Switch>
 
